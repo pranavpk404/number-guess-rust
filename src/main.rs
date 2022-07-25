91798e5a6a4c9d017115ffd8b0eead886d7c31ef
Pranav <pranavpk404@gmail.com>
2022-07-25 05:32:29 +0530

    Updated the version of rand and some minor changes

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
-    // Making an empty string which is mutable
-    let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-    // taking the input
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    println!("------------------- Guess the number -------------------");
+    let secret_number = rand::thread_rng().gen_range(1..10);
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number");
+    loop {
+        println!("Please input your guess.");
+        // Making an empty string which is mutable
+        let mut guess = String::new();
+        // taking the input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("secret_number:{secret_number}");
-    println!("You guessed: {guess}");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Small"),
-        Ordering::Equal => println!("You Win"),
-        Ordering::Greater => println!("Small"),
+        println!("secret_number:{secret_number}");
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Small"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+            Ordering::Greater => println!("Great"),
+        }
     }
 }
